2091317a22bd2d4f28e848607040ce2cc3f5ce20
Jason Arnold <jason.arnold.00@gmail.com>
2016-04-14 17:50:24 -0400

    trying to figure out JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+var groups = [];
+
 function Channel(displayName, game, logo, url) {
   this.displayName = displayName;
   this.game = game;
@@ -7,25 +9,24 @@ function Channel(displayName, game, logo, url) {
 
 function getChannels(){
   var arr = ["voyboy","imaqtpie","freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
-  var groups = [];
   arr.forEach(function(channel){
     $.getJSON('https://api.twitch.tv/kraken/channels/'+channel+'?callback=?', function(data) {
       var theChannel = new Channel(data['display_name'], data.game, data.logo, data.url);
-
+ 
       $.getJSON('https://api.twitch.tv/kraken/streams/'+channel+'?callback=?', function(data) {
         theChannel.status = data.stream;
-      });
-    groups.push(theChannel);    
-    });
+        groups.push(theChannel);
+      }); //end second getJSON   
+    }); //end first getJSON 
   }); //end forEach loop
-  return groups;
 }
 
-function buildHTML(groups) {
+function buildHTML() {
   console.log(groups);
   groups.forEach(function(group){
     $(".row").append("<p>"+group.displayName+"</p>");
   });
 }
 
-buildHTML(getChannels()); //what
+getChannels();
+window.setTimeout(buildHTML, 500);
