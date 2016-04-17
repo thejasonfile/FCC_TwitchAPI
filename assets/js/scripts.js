c7fa27393f2042ced03701c40c4ad5f96212e304
Jason Arnold <jason@thejasonfile.com>
2016-04-17 17:34:03 -0400

    finished basic JS and CSS'

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,59 +1,28 @@
-// var groups = [];
-
-function Channel(displayName, game, logo, url) {
-  this.displayName = displayName;
-  this.game = game;
-  this.logo = logo;
-  this.url = url;
-}
-
-// function getChannels(){
-//   var arr = ["voyboy","imaqtpie","freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
-//   arr.forEach(function(channel){
-//     $.getJSON('https://api.twitch.tv/kraken/channels/'+channel+'?callback=?', function(data) {
-//       console.log('getting data');
-//       var theChannel = new Channel(data['display_name'], data.game, data.logo, data.url);
-//       groups.push(theChannel);
-//       //$(".row").append("<p>"+data['display_name']+"</p>");
-//     }); //end getJSON 
-//   }); //end forEach loop
-//   buildHTML();
-// }
-
-// function buildHTML(){
-//   console.log('groups: ' + groups);
-// }
-
-// getChannels();
-
-var arr = ["voyboy","imaqtpie","freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+var arr = ["riotgames","imaqtpie","freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
 var groups = [];
 
 function getAJAX(){
   arr.forEach(function(twitchName){
-    $.ajax({
-      method: "GET",
-      url: "https://api.twitch.tv/kraken/channels/"+twitchName,
-      dataType: "jsonp",
-      async: false,
-      success: ajaxSucces,
-      error: function(){
-        console.log("error in AJAX get");
+    var HTMLString = "";
+    var link = 'https://www.twitch.tv/'+twitchName
+    $.getJSON('https://api.twitch.tv/kraken/streams/'+twitchName+'?callback=?', function(data) {
+      if(data.stream === null){
+        HTMLstring = "<div class='col-md-3 notRunning'><a href="+link+" target='_blank'>"+twitchName + " is not running</a></div>";
+        appendHTML(HTMLstring);
+      } else {
+        $.getJSON('https://api.twitch.tv/kraken/channels/'+twitchName+'?callback=?', function(data) {
+          console.log(data);
+          HTMLstring = "<div class='col-md-3 running'><a href="+link+" target='_blank'><img src='"+data.logo+"'>"+twitchName + " is running " + data.game+"</a></div>";
+          appendHTML(HTMLstring);
+        });
       }
-    }) //end ajax
-  }) //end forEach
+            
+    });
+  }); 
 }
 
-function ajaxSucces(data) {
-  var channelInfo = new Channel(data['display_name'], data.game, data.logo, data.url);
-  groups.push(channelInfo);
+function appendHTML(HTMLstring) {
+  $(".row").append(HTMLstring);
 }
-
-// function appendHTML() {
-//   groups.forEach(function(channel){
-//     console.log('appending: '+channel.displayName);
-//     $(".row").append("<div>"+channel.displayName+"</div>");
-//   });
-// }
 
 getAJAX();
