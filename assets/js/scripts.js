9ce277a905b7e0867f1e4076c889217e57151ccb
Jason Arnold <jason.arnold.00@gmail.com>
2016-04-20 11:47:15 -0400

    more CSS and JS changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,42 @@
-var arr = ["riotgames","imaqtpie","freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+var arr = ["comster404","cohhcarnage", "riotgames", "imaqtpie", "freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
 var groups = [];
 
 function getAJAX(){
   arr.forEach(function(twitchName){
-    var HTMLString = "";
+    var HTMLstring = "";
     var link = 'https://www.twitch.tv/'+twitchName
     $.getJSON('https://api.twitch.tv/kraken/streams/'+twitchName+'?callback=?', function(data) {
       if(data.stream === null){
-        HTMLstring = "<div class='col-md-3 notRunning'><a href="+link+" target='_blank'>"+twitchName + " is not running</a></div>";
-        appendHTML(HTMLstring);
+        $.getJSON('https://api.twitch.tv/kraken/channels/'+twitchName+'?callback=?', function(data) {
+          console.log(data);
+          HTMLstring = "<div class='col-md-8 notRunning'><div class='row'>";
+          HTMLstring += "<div class='col-md-2'><img src='"+data.logo+"'></div>";
+          HTMLstring += "<div class='col-md-4'><span class='twitchName'><a href="+link+" target='_blank'>"+twitchName+"</a></span></div>"
+          HTMLstring += "<div class='col-md-4'><span class='game'>"+data.game+"</span></div>";
+          HTMLstring += "</div></div></div>";
+          appendHTML(HTMLstring);
+        });
       } else {
         $.getJSON('https://api.twitch.tv/kraken/channels/'+twitchName+'?callback=?', function(data) {
           console.log(data);
-          HTMLstring = "<div class='col-md-3 running'><div class='row'><div class='col-md-4'>";
-          HTMLstring += "<img src='"+data.logo+"'>";
-          HTMLstring += "<div class='col-md-7'>";
-          HTMLstring += "<h1><a href="+link+" target='_blank'>"+twitchName+"</a></h1><p>"+data.game+"</p></div></div></div>";
-          appendHTML(HTMLstring);
+          HTMLstring = "<div class='col-md-8 running'><div class='row'>";
+          HTMLstring += "<div class='col-md-2'><img src='"+data.logo+"'></div>";
+          HTMLstring += "<div class='col-md-4'><span class='twitchName'><a href="+link+" target='_blank'>"+twitchName+"</a></span></div>"
+          HTMLstring += "<div class='col-md-4'><span class='game'>"+data.game+"</span></div>";
+          HTMLstring += "</div></div></div>";
+          prependHTML(HTMLstring);
         });
-      }
-            
+      }     
     });
   }); 
 }
 
 function appendHTML(HTMLstring) {
-  $(".row").append(HTMLstring);
+  $(".wrapper").append(HTMLstring);
+}
+
+function prependHTML(HTMLstring) {
+  $(".wrapper").prepend(HTMLstring);
 }
 
 getAJAX();
