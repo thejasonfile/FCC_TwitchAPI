5d90be8d4a766511b6e9f94f47497650c9e061ad
Jason Arnold <jason.arnold.00@gmail.com>
2016-04-20 14:26:33 -0400

    refactored JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,43 +1,44 @@
-var arr = ["OgamingSC2","comster404","cohhcarnage", "riotgames", "imaqtpie", "freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
-var groups = [];
+var arr = ["OgamingSC2","comster404","cohhcarnage", "riotgames", "imaqtpie", "freecodecamp", 
+            "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
 
 function getAJAX(){
   arr.forEach(function(twitchName){
     var HTMLstring = "";
     var imgSource = "";
+    var stream = "";
     var link = 'https://www.twitch.tv/'+twitchName
     $.getJSON('https://api.twitch.tv/kraken/streams/'+twitchName+'?callback=?', function(data) {
-      if(data.stream === null){
-        $.getJSON('https://api.twitch.tv/kraken/channels/'+twitchName+'?callback=?', function(data) {
-          if (data.logo === null) {
-            imgSource = "../TwitchAPI/assets/img/redx.png";
-          } else {
-            imgSource = data.logo;
-          }
-          buildHTML('notRunning', imgSource, link, twitchName);
-        });
-      } else if (data.status === 422) {
-          buildHTML('undefined', '../TwitchAPI/assets/img/redx.png', link, twitchName);
-      } else {
-        $.getJSON('https://api.twitch.tv/kraken/channels/'+twitchName+'?callback=?', function(data) {
-          if (data.logo === null) {
-            imgSource = "../TwitchAPI/assets/img/redx.png";
-          } else {
-            imgSource = data.logo;
-          }
-          buildHTML('running', imgSource, link, twitchName);
-        });
-      }     
-    });
-  }); 
-}
+        if (data.stream === null){
+          stream = null;
+        }
+      $.getJSON('https://api.twitch.tv/kraken/channels/'+twitchName+'?callback=?', function(data) {
+        if (data.logo === null) {
+          imgSource = "../TwitchAPI/assets/img/redx.png";
+        } else {
+          imgSource = data.logo;
+        }
 
-function buildHTML(className, imgSource, link, twitchName) {
+        if (stream === null){
+          buildHTML('notRunning', imgSource, link, twitchName, 'Offline');
+        }
+        else if (data.status === 422) {
+          buildHTML('undefined', '../TwitchAPI/assets/img/redx.png', link, twitchName, 'Not valid');
+        }
+        else {
+          buildHTML('running', imgSource, link, twitchName, data.game);
+        }
+      });
+    });
+  });
+};
+
+function buildHTML(className, imgSource, link, twitchName, game) {
   HTMLstring = "<div class='col-md-8 "+className+"'><div class='row'>";
   HTMLstring += "<div class='col-md-2'><img src='"+imgSource+"'></div>";
   HTMLstring += "<div class='col-md-4'><span class='twitchName'><a href="+link+" target='_blank'>"+twitchName+"</a></span></div>"
-  HTMLstring += "<div class='col-md-4'><span class='game'>Offline</span></div>";
+  HTMLstring += "<div class='col-md-4'><span class='game'>"+game+"</span></div>";
   HTMLstring += "</div></div></div>";
+  
   if(className === 'running'){
     prependHTML(HTMLstring);
   } else {
